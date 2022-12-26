231292446bb161bc25efb9bb351f39ee9db9568a
jzelAdmin2006 <jo.zeller@protonmail.com>
2022-12-26 10:18:08 +0100

    define basic artist concept

--- src/main/java/tech/bison/trainee2021/Artist.java
+++ src/main/java/tech/bison/trainee2021/Artist.java
@@ -1,5 +1,16 @@
 package tech.bison.trainee2021;
 
+import java.util.List;
+
 public class Artist {
 
+  private List<User> members;
+
+  public Artist(List<User> members) {
+    this.members = members;
+  }
+
+  public List<User> getMembers() {
+    return members;
+  }
 }
---
+++ src/test/java/tech/bison/trainee2021/ArtistTest.java
@@ -0,0 +1,36 @@
+package tech.bison.trainee2021;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class ArtistTest {
+  @Test
+  void newArtistWithMember_getMembers_isTheSame() {
+    User member = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    List<User> members = new ArrayList<>();
+    members.add(member);
+    Artist artist = new Artist(members);
+
+    List<User> result = artist.getMembers();
+
+    assertThat(result).containsExactly(member);
+  }
+
+  @Test
+  void newArtistWithMembers_getMembers_isTheSame() {
+    User member1 = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    User member2 = new User("jzelAdmin2006_2", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    List<User> members = new ArrayList<>();
+    members.add(member1);
+    members.add(member2);
+    Artist artist = new Artist(members);
+
+    List<User> result = artist.getMembers();
+
+    assertThat(result).containsExactly(member1, member2);
+  }
+}
