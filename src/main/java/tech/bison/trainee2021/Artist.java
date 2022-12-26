9a9142dbc7137eec9f28abe732fdb060fa8478e9
jzelAdmin2006 <jo.zeller@protonmail.com>
2022-12-26 11:22:25 +0100

    add basic artist functionality

--- src/main/java/tech/bison/trainee2021/Artist.java
+++ src/main/java/tech/bison/trainee2021/Artist.java
@@ -1,16 +1,109 @@
 package tech.bison.trainee2021;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Artist {
 
-  private List<User> members;
+  private final int id;
+  private List<User> members = new ArrayList<>();
+  private String name;
 
-  public Artist(List<User> members) {
-    this.members = members;
+  public Artist(List<User> members, String name) {
+    id = create(name);
+    addMembers(members);
+    this.name = name;
+  }
+
+  private void addMembers(List<User> members) {
+    this.members.addAll(members);
+    for (User member : members) {
+      addMember(member);
+    }
+  }
+
+  private int create(String name) {
+    int id = 0;
+    try {
+      Connection connection = DriverManager.getConnection(Commusify.DATABASE);
+      CallableStatement callableStatement = connection.prepareCall("{call SP_CREATE_ARTIST(?)}");
+      callableStatement.setString(1, name);
+      ResultSet result = callableStatement.executeQuery();
+
+      result.next();
+      id = result.getInt("ID");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return id;
+  }
+
+  private void addMember(User member) {
+    try {
+      Connection connection = DriverManager.getConnection(Commusify.DATABASE);
+      CallableStatement callableStatement = connection.prepareCall("{call SP_ADD_ARTIST_MEMBER(?, ?)}");
+      callableStatement.setInt(1, id);
+      callableStatement.setInt(2, member.getId());
+      callableStatement.execute();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Artist(int id) {
+    this.id = id;
+    find(id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, members, name);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Artist other = (Artist) obj;
+    return id == other.id && Objects.equals(members, other.members) && Objects.equals(name, other.name);
+  }
+
+  private void find(int id) {
+    try {
+      Connection connection = DriverManager.getConnection(Commusify.DATABASE);
+      CallableStatement callableStatement = connection.prepareCall("{call SP_FIND_ARTIST(?)}");
+      callableStatement.setInt(1, id);
+      ResultSet result = callableStatement.executeQuery();
+
+      result.next();
+      name = result.getString("Name");
+      do {
+        members.add(new User(result.getInt("FK_UserID")));
+      } while (result.next());
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   public List<User> getMembers() {
     return members;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
   }
 }
--- src/test/java/tech/bison/trainee2021/ArtistTest.java
+++ src/test/java/tech/bison/trainee2021/ArtistTest.java
@@ -13,7 +13,7 @@ public class ArtistTest {
     User member = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
     List<User> members = new ArrayList<>();
     members.add(member);
-    Artist artist = new Artist(members);
+    Artist artist = new Artist(members, "jzelAdmin2006Artist");
 
     List<User> result = artist.getMembers();
 
@@ -27,10 +27,62 @@ public class ArtistTest {
     List<User> members = new ArrayList<>();
     members.add(member1);
     members.add(member2);
-    Artist artist = new Artist(members);
+    Artist artist = new Artist(members, "jzelAdmin2006Artist and jzelAdmin2006_2Artist");
 
     List<User> result = artist.getMembers();
 
     assertThat(result).containsExactly(member1, member2);
+  }
+
+  @Test
+  void newArtistWithName_getName_isTheSame() {
+    User member = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    List<User> members = new ArrayList<>();
+    members.add(member);
+    Artist artist = new Artist(members, "jzelAdmin2006Artist");
+
+    String result = artist.getName();
+
+    assertThat(result).isEqualTo("jzelAdmin2006Artist");
+  }
+
+  @Test
+  void newArtistWithDifferentName_getName_isTheSame() {
+    User member = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    List<User> members = new ArrayList<>();
+    members.add(member);
+    Artist artist = new Artist(members, "jzelAdmin2006Artist2");
+
+    String result = artist.getName();
+
+    assertThat(result).isEqualTo("jzelAdmin2006Artist2");
+  }
+
+  @Test
+  void newArtist_artistWithSameId_isEqual() {
+    User member = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    List<User> members = new ArrayList<>();
+    members.add(member);
+    Artist artist = new Artist(members, "jzelAdmin2006Artist");
+    Artist artistWithSameId = new Artist(artist.getId());
+
+    assertThat(artist).isEqualTo(artistWithSameId);
+    assertThat(artist.getMembers()).containsAll(artistWithSameId.getMembers()).containsExactly(member);
+    assertThat(artist.getName()).isEqualTo(artistWithSameId.getName()).isEqualTo("jzelAdmin2006Artist");
+  }
+
+  @Test
+  void newDifferentArtist_artistWithSameId_isEqual() {
+    User member1 = new User("jzelAdmin2006", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    User member2 = new User("jzelAdmin2006_2", "TopsecretPassword123456", "Jzel2", "Admin", "jzel2006@admin.ch");
+    List<User> members = new ArrayList<>();
+    members.add(member1);
+    members.add(member2);
+    Artist artist = new Artist(members, "jzelAdmin2006Artist");
+    Artist artistWithSameId = new Artist(artist.getId());
+
+    assertThat(artist).isEqualTo(artistWithSameId);
+    assertThat(artist.getMembers()).containsAll(artistWithSameId.getMembers()).containsExactly(member1, member2);
+    assertThat(artist.getName()).isEqualTo(artistWithSameId.getName()).isEqualTo("jzelAdmin2006Artist");
   }
 }
