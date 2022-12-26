2d05a735ae047ef35da6b2960e14b901e7056eba
jzelAdmin2006 <jo.zeller@protonmail.com>
2022-12-26 14:40:26 +0100

    remove untested track functionality

--- src/main/java/tech/bison/trainee2021/playable/Track.java
+++ src/main/java/tech/bison/trainee2021/playable/Track.java
@@ -1,53 +1,5 @@
 package tech.bison.trainee2021.playable;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import tech.bison.trainee2021.Commusify;
-import tech.bison.trainee2021.Genre;
-
 public class Track {
-  private final int id;
-  private String title;
-  private byte[] audio;
-  private Genre genre;
 
-  public Track(int id) {
-    this.id = id;
-    find(id);
-  }
-
-  private void find(int id) {
-    try {
-      Connection connection = DriverManager.getConnection(Commusify.DATABASE);
-      Statement statement = connection.createStatement();
-      ResultSet result = statement.executeQuery("SP_FIND_TRACK");
-
-      result.next();
-      title = result.getString("Title");
-      audio = result.getBytes("Audio");
-      genre = new Genre(result.getInt("FK_GenreID"));
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public byte[] getAudio() {
-    return audio;
-  }
-
-  public Genre getGenre() {
-    return genre;
-  }
 }
