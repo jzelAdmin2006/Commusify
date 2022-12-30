0c98f34ef3df17d7a716479ea8ecb049d2046080
jzelAdmin2006 <jo.zeller@protonmail.com>
2022-12-30 09:17:33 +0100

    give user ability to add multiple albuminterpreters

--- src/main/java/tech/bison/trainee2021/userInterface/command/create/CreateRecordAlbum.java
+++ src/main/java/tech/bison/trainee2021/userInterface/command/create/CreateRecordAlbum.java
@@ -4,6 +4,7 @@ import static tech.bison.trainee2021.userInterface.util.NumericChecker.isNumeric
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import tech.bison.trainee2021.playable.Playable;
 import tech.bison.trainee2021.playable.Track;
@@ -19,12 +20,6 @@ import tech.bison.trainee2021.structure.Artist.ArtistIdChecker;
 import tech.bison.trainee2021.userInterface.UserInterface;
 import tech.bison.trainee2021.userInterface.command.argumentExpectation.MinimumArgumentAmountExpectation;
 
-// TODO Currently only one interpreter can be added. The remaining arguments are simply ignored. For
-// reasons of time, I'm leaving it that way for this project at the moment, but this it could be
-// added
-// at any time.
-
-@Deprecated
 public class CreateRecordAlbum implements MinimumArgumentAmountExpectation {
 
   @Override
@@ -38,34 +33,38 @@ public class CreateRecordAlbum implements MinimumArgumentAmountExpectation {
   }
 
   private String processNextArgument(List<String> arguments, int trackId) {
-    String interpreterId = arguments.get(3);
-    if (isNumeric(interpreterId)) {
-      return createAlbumWhenArgumentsChecked(arguments, trackId, Integer.parseInt(interpreterId));
-    } else {
-      return String.format("\"%s\" is not an artist ID (an artist ID should be a number).", interpreterId);
+    List<String> interpreterIds = arguments.subList(3, arguments.size());
+    for (String interpreterId : interpreterIds) {
+      if (!isNumeric(interpreterId)) {
+        return String.format("\"%s\" is not an artist ID (an artist ID should be a number).", interpreterId);
+      }
     }
+    return createAlbumWhenArgumentsChecked(arguments,
+        trackId,
+        interpreterIds.stream().map(interpreterId -> Integer.parseInt(interpreterId)).collect(Collectors.toList()));
   }
 
-  private String createAlbumWhenArgumentsChecked(List<String> arguments, int trackId, int interpreterId) {
+  private String createAlbumWhenArgumentsChecked(List<String> arguments, int trackId, List<Integer> interpreterIds) {
     if (new TrackIdChecker().exists(trackId)) {
-      return checkNextArgument(arguments, new Track(trackId), interpreterId);
+      return checkNextArgument(arguments, new Track(trackId), interpreterIds);
     } else {
       return String.format("The track ID %s wan't found.", trackId);
     }
   }
 
-  private String checkNextArgument(List<String> arguments, Track track, int interpreterId) {
-    if (new ArtistIdChecker().exists(interpreterId)) {
-      Artist artistUserClaimsToBe = new Artist(interpreterId);
-      if (UserInterface.getCurrentUser().isArtistMember(artistUserClaimsToBe)) {
-        return createAlbum(arguments,
-            Collections.singletonList(track),
-            Collections.singletonList(artistUserClaimsToBe));
-      } else {
-        return String.format("You aren't a member of the artist with the id \"%s\"", artistUserClaimsToBe.getId());
+  private String checkNextArgument(List<String> arguments, Track track, List<Integer> interpreterIds) {
+    for (int interpreterId : interpreterIds) {
+      if (!new ArtistIdChecker().exists(interpreterId)) {
+        return String.format("The artist ID %s wan't found.", interpreterId);
       }
+    }
+    Artist artistUserClaimsToBe = new Artist(interpreterIds.get(0));
+    if (UserInterface.getCurrentUser().isArtistMember(artistUserClaimsToBe)) {
+      return createAlbum(arguments,
+          Collections.singletonList(track),
+          interpreterIds.stream().map(interpreterId -> new Artist(interpreterId)).collect(Collectors.toList()));
     } else {
-      return String.format("The artist ID %s wan't found.", interpreterId);
+      return String.format("You aren't a member of the artist with the id \"%s\"", artistUserClaimsToBe.getId());
     }
   }
 
