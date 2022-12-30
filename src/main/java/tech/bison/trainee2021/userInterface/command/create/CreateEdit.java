95beeabd55aac03f5e6bc2c5b72d28230376928c
jzelAdmin2006 <jo.zeller@protonmail.com>
2022-12-30 09:18:02 +0100

    specify message

--- src/main/java/tech/bison/trainee2021/userInterface/command/create/CreateEdit.java
+++ src/main/java/tech/bison/trainee2021/userInterface/command/create/CreateEdit.java
@@ -1,6 +1,7 @@
 package tech.bison.trainee2021.userInterface.command.create;
 
-// TODO This command is unimplemented for reasons of time. However, the Commusify framework is built
+// TODO This command (for creating mashups and remixes) is unimplemented for reasons of time.
+// However, the Commusify framework is built
 // in such a
 // way that it would be possible to add it at any time.
 public class CreateEdit {
